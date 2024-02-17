bf55bf3cb920d14fa63bc450e8667f660f9b5bf9
maurya-anand <anandmaurya@hotmail.com>
2024-02-17 19:28:30 +0000

    Refactor test_data/download.sh script to include downsampling of fastq file

--- test_data/download.sh
+++ test_data/download.sh
@@ -8,11 +8,11 @@ if [ ! -f "Homo_sapiens.GRCh38.dna.chromosome.22.fa" ]; then
     curl https://ftp.ensembl.org/pub/release-111/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz --output Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz; \
     gunzip Homo_sapiens.GRCh38.dna.chromosome.22.fa.gz
 fi
-
-# downsampling the downloaded fastq
-seqtk sample HG002.fastq.gz 0.1 > HG002-downsample-0.1.fastq
-gzip HG002-downsample-0.1.fastq
-
-echo "Test data downloaded in: $SCRIPT_DIR"
-echo "If you intend to run the entire pipeline, including the annotation step, please specify the VEP cache directory. If the annotation step is not required, you can run the appropriate target as shown in the help message."
-echo "Try: make reads_fastq_gz=./test_data/HG002-downsample-0.1.fastq.gz file_label=HG002 genome_ref=./test_data/Homo_sapiens.GRCh38.dna.chromosome.22.fa vep_cache=./vep_cache threads=4"
+if [ ! -f "HG002-downsample-0.1.fastq.gz" ]; then
+    # downsampling the downloaded fastq
+    seqtk sample HG002.fastq.gz 0.1 > HG002-downsample-0.1.fastq
+    gzip HG002-downsample-0.1.fastq
+fi
+printf "\nTest data downloaded in: $SCRIPT_DIR\n";
+printf "\nIf you intend to run the entire pipeline, including the annotation step, please specify the VEP cache directory. If the annotation step is not required, you can run the appropriate target as shown in the help message.\n";
+printf "\nTry:\nmake reads_fastq_gz=$SCRIPT_DIR/test_data/HG002-downsample-0.1.fastq.gz file_label=HG002 genome_ref=$SCRIPT_DIR/test_data/Homo_sapiens.GRCh38.dna.chromosome.22.fa vep_cache=<path_to_vep_cache> threads=4\n";
